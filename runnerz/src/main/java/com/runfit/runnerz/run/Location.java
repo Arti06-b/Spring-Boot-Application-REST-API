7de4eb53a476c83c15879b98ca1757decea6574f
Arti Babar <aratibabar15@gmail.com>
2025-01-20 01:11:47 +0530

    Delete runnerz directory

--- runnerz/src/main/java/com/runfit/runnerz/run/Location.java
+++
@@ -1,5 +0,0 @@
-package com.runfit.runnerz.run;
-
-public enum Location {
-    INDOOR ,OUTDOOR
-}
--- runnerz/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-Insert into  Run (id, title , started_on , completed_on , miles, location)
-values (1, 'Morning Run', '2017-01-01 06:00:00' , '2017-01-01 07:00:00',5 , 'OUTDOOR');
